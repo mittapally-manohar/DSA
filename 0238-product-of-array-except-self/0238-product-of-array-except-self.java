0be8d732d0cb668e121a2acf8ba4f9dd587aa031
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2024-12-11 01:52:09 +0530

    Time: 2 ms (89.35%), Space: 56.1 MB (25.25%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -5,11 +5,17 @@ class Solution {
          for (int i = 1; i < nums.length; i++) {
             ans[i] = ans[i - 1] * nums[i - 1];
          }
-         int rightProduct = 1;
-         for (int i = nums.length -1; i >=0; i--) {
-            ans[i] *= rightProduct;
-            rightProduct *= nums[i];
+         
+         int[] ans1 = new int[nums.length];
+         ans1[nums.length-1] = 1;
+         for (int i = nums.length - 2; i >= 0; i--) {
+            ans1[i] = ans1[i + 1] * nums[i + 1];
          }
-         return ans;
+
+         int[] res = new int[nums.length];
+         for (int i = 0; i < nums.length; i++) {
+            res[i] = ans[i] * ans1[i];
+         }
+         return res;
     }
 }

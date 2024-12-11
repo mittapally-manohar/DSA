c34e3ad92e54a22fd1dfb366cadfe59ababd86d4
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2024-12-11 12:47:47 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.16%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -5,18 +5,13 @@ class Solution {
         int p = m + n - 1;
         while (p1 >= 0 && p2 >= 0) {
             if (nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1];
-                p1--;
+                nums1[p--] = nums1[p1--];
             } else {
-                nums1[p] = nums2[p2];
-                p2--;
+                nums1[p--] = nums2[p2--];
             }
-            p--;
         }
-        while (p2 >= 0) {
-            nums1[p] = nums2[p2];
-            p--;
-            p2--;
+        while (p2 >=0 ){
+            nums1[p--] = nums2[p2--];
         }
     }
 }

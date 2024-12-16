0af6899e5cf6fd1b24ee235d013fb1eb5ddc0e45
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2024-12-16 10:48:17 +0530

    Time: 0 ms (100%), Space: 40.8 MB (11.04%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -2,7 +2,7 @@ class Solution {
         public int fib(int n) {
           int oldState = 0;
           int newState = 1;
-          for (int i = 2; i <= n + 1; i++) {
+          for (int i = 2; i <= n+1; i++) {
             int curState = oldState + newState;
             oldState = newState;
             newState = curState;

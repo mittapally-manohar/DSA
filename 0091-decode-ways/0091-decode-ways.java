722beb94473625b89dc9cf6edbc771ca8df147b8
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2024-12-16 20:09:35 +0530

    Time: 1 ms (80.83%), Space: 41.9 MB (32.09%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,32 +1,22 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s.length() == 0 || s == null || s.charAt(0) == '0') {
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
             return 0;
         }
-        int[] memo = new int[s.length()];
-        Arrays.fill(memo, -1);
-        return decode(0, s, memo);
-    }
-
-    public int decode(int i, String s, int[] memo) {
-        if (i == s.length()) {
-            return 1;
-        }
-        if (s.charAt(i) == '0') {
-            return 0;
-        }
-        if (memo[i] != -1) {
-            return memo[i];
-        }
-        int ways = decode(i + 1, s, memo);
-
-        if (i < s.length()-1) {
-            int twoDigit = Integer.parseInt(s.substring(i, i+2));
-            if (twoDigit >= 10 && twoDigit <= 26) {
-                ways += decode(i+2, s, memo);
+        int oldState = 1;
+        int newState = 1;
+        for (int i = 1; i < s.length(); i++) {
+            int count = 0;
+            if (s.charAt(i) != '0') {
+                count = newState;
             }
+            int twoDigit = Integer.parseInt(s.substring(i-1, i+1));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                count += oldState;
+            }
+            oldState = newState;
+            newState = count;
         }
-        memo[i] = ways;
-        return ways;
+        return newState;
     }
 }

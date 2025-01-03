ad89caa05c2d1acbc125991c8145c1f1c041519a
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-01-03 18:12:16 +0530

    Time: 0 ms (100%), Space: 41.3 MB (28.72%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,7 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         int oldState = 0, newState = 0;
         for (int num : nums) {
-            newState = Math.max(oldState + num, oldState = newState);
+            newState = Math.max(num + oldState, oldState = newState);
         }
         return newState;
     }

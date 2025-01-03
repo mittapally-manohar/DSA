d3099f9059398c098f05539039207489fca50a71
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-01-03 18:18:23 +0530

    Time: 1 ms (81.17%), Space: 41.6 MB (84.97%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -3,15 +3,14 @@ class Solution {
         if (s == null || s.length() == 0 || s.charAt(0) == '0') {
             return 0;
         }
-        int oldState = 1;
-        int newState = 1;
+        int oldState = 1, newState = 1;
         for (int i = 1; i < s.length(); i++) {
             int count = 0;
-            if (s.charAt(i) != '0') {
+            if (s.charAt(i) != '0'){
                 count = newState;
             }
-            int twoDigit = Integer.parseInt(s.substring(i-1, i+1));
-            if (twoDigit >= 10 && twoDigit <= 26) {
+            int twoDigit = Integer.parseInt(s.substring(i - 1, i + 1));
+            if (twoDigit >= 10 && twoDigit <=26) {
                 count += oldState;
             }
             oldState = newState;

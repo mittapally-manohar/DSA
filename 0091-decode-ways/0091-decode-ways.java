7de0d83c582248b8450ee2b6f4970ca76057390a
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-01-05 10:50:31 +0530

    Time: 1 ms (81.3%), Space: 41.8 MB (65.29%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,21 +1,21 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
-            return 0;
+       if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+        return 0;
+       }
+       int oldState = 1, newState = 1;
+       for (int i = 1; i < s.length(); i++ ) {
+        int count = 0;
+        if (s.charAt(i) != '0') {
+            count = newState;
         }
-        int oldState = 1, newState = 1;
-        for (int i = 1; i < s.length(); i++) {
-            int count = 0;
-            if (s.charAt(i) != '0'){
-                count = newState;
-            }
-            int twoDigit = Integer.parseInt(s.substring(i - 1, i + 1));
-            if (twoDigit >= 10 && twoDigit <=26) {
-                count += oldState;
-            }
-            oldState = newState;
-            newState = count;
+        int twoDigit = Integer.parseInt(s.substring(i - 1, i + 1));
+        if (twoDigit >= 10 && twoDigit <= 26) {
+            count += oldState;
         }
-        return newState;
+        oldState = newState;
+        newState = count;
+       }
+       return newState;
     }
 }

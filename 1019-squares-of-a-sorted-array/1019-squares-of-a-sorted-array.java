18c18bb3ad046fe3f18964c89eecab32d0d29520
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-02-19 23:50:07 +0530

    Time: 5 ms (47.24%), Space: 45.5 MB (99.65%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,15 +1,44 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-       int[] res = new int[nums.length];
-       for(int left = 0, right = nums.length-1, count = nums.length-1; left <= right;) {
-            int leftSquare = nums[left] * nums[left];
-            int rightSquare = nums[right] * nums[right];
-            if(leftSquare > rightSquare) {
-                res[count--] = leftSquare; left++;
-            }else{
-                res[count--] = rightSquare; right--;
+       List<Integer> squaredList = generateSortedSquares(nums);
+        return squaredList.stream().mapToInt(i -> i).toArray();
+    }
+    public List<Integer> generateSortedSquares(int[] nums) {
+        int left = 0, right = nums.length;
+        
+        // Binary search to find the first non-negative number
+        while (right - left > 1) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > 0) {
+                right = mid;
+            } else {
+                left = mid;
             }
-       }
-       return res;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        
+        // Merge squares from left (negative side) and right (positive side)
+        while (left >= 0 || right < nums.length) {
+            if (left < 0) {
+                result.add(nums[right] * nums[right]);
+                right++;
+            } else if (right >= nums.length) {
+                result.add(nums[left] * nums[left]);
+                left--;
+            } else {
+                int leftSquare = nums[left] * nums[left];
+                int rightSquare = nums[right] * nums[right];
+                if (leftSquare < rightSquare) {
+                    result.add(leftSquare);
+                    left--;
+                } else {
+                    result.add(rightSquare);
+                    right++;
+                }
+            }
+        }
+
+        return result;
     }
 }

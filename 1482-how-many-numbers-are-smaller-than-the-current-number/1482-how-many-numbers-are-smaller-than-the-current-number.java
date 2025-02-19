2f526a73ff3c022ffa368ad722064fa4e696df50
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-02-20 01:47:59 +0530

    Time: 1 ms (100%), Space: 44.8 MB (23.63%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
@@ -2,14 +2,19 @@ class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] copy = (int[])nums.clone();
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++) {
-            map.putIfAbsent(nums[i],i);
+        int[] counter = new int[101];
+        for (int i = 0; i < nums.length; i++) {
+            counter[nums[i]]++;
         }
-        for(int i = 0; i < copy.length; i++) {
-            res[i] = map.get(copy[i]);
+        for (int i = 1; i < 101; i++) {
+            counter[i] = counter[i - 1] + counter[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                res[i] = 0;
+            } else {
+                res[i] = counter[nums[i] - 1];
+            }
         }
         return res;
     }

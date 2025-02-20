a64dcd6686e7fd1504d1cb94a9716e5d7dba1765
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-02-20 17:41:53 +0530

    Time: 26 ms (92.09%), Space: 51.9 MB (37.49%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,25 +2,22 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         int n = nums.length;
-        Arrays.sort(nums);
-        for(int i = 0; i < n-2; i++){
-            if(i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            int j = i + 1;
-            int k = n - 1;
+        Arrays.sort(nums); // Sort the array
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate `i`
+
+            int j = i + 1, k = n - 1;
             while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-                if (total > 0) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0) {
                     k--;
-                } else if (total < 0) {
+                } else if (sum < 0) {
                     j++;
                 } else {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    while (j < k && nums[j] == nums[j-1]) {
-                        j++;
-                    }
+                    res.add(List.of(nums[i], nums[j], nums[k])); // More efficient than Arrays.asList()
+                    while (++j < k && nums[j] == nums[j - 1]); // Skip duplicate `j`
+                    while (--k > j && nums[k] == nums[k + 1]); // Skip duplicate `k`
                 }
             }
         }

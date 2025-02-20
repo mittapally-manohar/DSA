b7bb10c816a1340846941dfa0d278b74c434702a
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-02-20 10:29:29 +0530

    Time: 5 ms (34.82%), Space: 52.5 MB (12.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,5 +1,6 @@
 class Solution {
-    private static final int[][] DIRECTIONS = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+    private static final int[][] DIRECTIONS = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
     public int numIslands(char[][] grid) {
        if(grid == null || grid.length == 0) {
         return 0;
@@ -7,30 +8,35 @@ class Solution {
        int count = 0;
        int row = grid.length;
        int cols = grid[0].length;
-       Queue<int[]> queue = new LinkedList<>();
+      
        for(int i = 0; i < row; i++) {
         for(int j = 0; j < cols; j++) {
             if (grid[i][j] == '1') {
                 count++;
-                queue.offer(new int[]{i,j});
-                grid[i][j] = '0';
+                 Queue<int[]> queue = new LinkedList<>();
+                bfs(grid,i,j,queue);
+            }
+        }
+       }
+        return count;
+    }
 
-                while(!queue.isEmpty()){
-                    int[] cell = queue.poll();
+    public void bfs(char[][] grid, int i, int j, Queue<int[]> queue) {
+        int row = grid.length;
+        int cols = grid[0].length;
+        queue.offer(new int[]{i,j});
+        grid[i][j] = '0';
+        if(!queue.isEmpty()){
+                int[] cell = queue.poll();
                     for(int[] dir : DIRECTIONS){
                         int newX = cell[0] + dir[0];
                         int newY = cell[1] + dir[1];
                         if(newX >= 0 && newY >=0 && newX < row && newY < cols) {
                             if(grid[newX][newY]=='1'){
-                                queue.offer(new int[]{newX,newY});
-                                grid[newX][newY] = '0';
+                                bfs(grid, newX, newY, queue);
                             }
                         }
-                    }
-                }
             }
         }
-       }
-        return count;
     }
 }

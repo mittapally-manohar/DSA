e7228780ec91c9ac714ddbf418321b737835fedd
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-02-20 10:24:02 +0530

    Time: 7 ms (19.38%), Space: 48.6 MB (98.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,37 +1,36 @@
 class Solution {
-    private static final int[][] DIRECTIONS = {{1,0},{-1,0},{0,1},{0,-1}};
+    private static final int[][] DIRECTIONS = {{1,0}, {-1,0}, {0,1}, {0,-1}};
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0) return 0;
+       if(grid == null || grid.length == 0) {
+        return 0;
+       }
+       int count = 0;
+       int row = grid.length;
+       int cols = grid[0].length;
+       Queue<int[]> queue = new LinkedList<>();
+       for(int i = 0; i < row; i++) {
+        for(int j = 0; j < cols; j++) {
+            if (grid[i][j] == '1') {
+                count++;
+                queue.offer(new int[]{i,j});
+                grid[i][j] = '0';
 
-        int count = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
-        Queue<int[]> queue = new LinkedList<>();
-
-        for(int i=0; i<rows; i++) {
-            for(int j=0; j<cols; j++) {
-                if(grid[i][j] == '1'){
-                    count++;
-                    queue.offer(new int[]{i,j});
-                    grid[i][j] = '0';
-                    while(!queue.isEmpty()){
-                        int[] cell = queue.poll();
-                        for(int[] dir : DIRECTIONS) {
-                            int newX = cell[0] + dir[0];
-                            int newY = cell[1] + dir[1];
-
-                            if(newX >= 0 && newY >=0 &&
-                                newX < rows && newY < cols){
-                                    if(grid[newX][newY] == '1') {
-                                        queue.offer(new int[]{newX, newY});
-                                        grid[newX][newY] = '0';
-                                    }
-                                }
+                while(!queue.isEmpty()){
+                    int[] cell = queue.poll();
+                    for(int[] dir : DIRECTIONS){
+                        int newX = cell[0] + dir[0];
+                        int newY = cell[1] + dir[1];
+                        if(newX >= 0 && newY >=0 && newX < row && newY < cols) {
+                            if(grid[newX][newY]=='1'){
+                                queue.offer(new int[]{newX,newY});
+                                grid[newX][newY] = '0';
+                            }
                         }
                     }
                 }
             }
         }
+       }
         return count;
     }
 }

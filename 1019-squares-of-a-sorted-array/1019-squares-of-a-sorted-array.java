62e83cb425f3078f93332fef69e2505ab7020863
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-02-21 18:47:59 +0530

    Time: 1 ms (100%), Space: 47.5 MB (34.53%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,44 +1,20 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-       List<Integer> squaredList = generateSortedSquares(nums);
-        return squaredList.stream().mapToInt(i -> i).toArray();
-    }
-    public List<Integer> generateSortedSquares(int[] nums) {
-        int left = 0, right = nums.length;
-        
-        // Binary search to find the first non-negative number
-        while (right - left > 1) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > 0) {
-                right = mid;
-            } else {
-                left = mid;
+        int n = nums.length;
+        int[] res = new int[n];
+        int i = 0;
+        int j = n-1, k = n-1;
+        while(i <= j) {
+            int x = nums[i] * nums[i];
+            int y = nums[j] * nums[j];
+            if( x > y) {
+                i++;
+                res[k--] = x;
+            }else {
+                j--;
+                res[k--] = y;
             }
         }
-
-        List<Integer> result = new ArrayList<>();
-        
-        // Merge squares from left (negative side) and right (positive side)
-        while (left >= 0 || right < nums.length) {
-            if (left < 0) {
-                result.add(nums[right] * nums[right]);
-                right++;
-            } else if (right >= nums.length) {
-                result.add(nums[left] * nums[left]);
-                left--;
-            } else {
-                int leftSquare = nums[left] * nums[left];
-                int rightSquare = nums[right] * nums[right];
-                if (leftSquare < rightSquare) {
-                    result.add(leftSquare);
-                    left--;
-                } else {
-                    result.add(rightSquare);
-                    right++;
-                }
-            }
-        }
-
-        return result;
+        return res;
     }
 }

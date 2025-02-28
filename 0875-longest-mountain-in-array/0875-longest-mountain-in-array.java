adb4d62806601e5e676f309f1444f08f0e689ebe
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-02-28 16:28:25 +0530

    Time: 3 ms (61.59%), Space: 45.1 MB (51.01%) - LeetHub

--- 0875-longest-mountain-in-array/0875-longest-mountain-in-array.java
+++ 0875-longest-mountain-in-array/0875-longest-mountain-in-array.java
@@ -1,23 +1,27 @@
 class Solution {
     public int longestMountain(int[] arr) {
         int max = 0, n = arr.length, i = 1;
-       
         while (i < n) {
-            int up = 0, down = 0;
-            while (i < n && arr[i - 1] < arr[i]) {
-                i++;
-                up++;
-            }
-            while (i < n && arr[i - 1] > arr[i]) {
-                i++;
-                down++;
-            }
-            if (up > 0 && down > 0){
-                max = Math.max(max, up + down + 1);
-            }
-            while (i < n && arr[i - 1] == arr[i]) {
-                i++;
-            }
+           //climb up
+           int up = 0;
+           while(i < n && arr[i-1] < arr[i]){
+            i++;
+            up++;
+           }
+           //climb down
+           int down = 0;
+           while(i < n && arr[i-1] > arr[i]){
+            i++;
+            down++;
+           }
+           //climb flat
+           while(i < n && arr[i-1] == arr[i]){
+            i++;
+           }
+           //calculate max if applicable
+           if(up > 0 && down > 0) {
+            max = Math.max(max, up+down+1);
+           }
         }
         return max;
     }

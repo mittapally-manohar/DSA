db21d109d1694dfe6900fc7f5f9038335a505317
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-03-06 19:28:10 +0530

    Time: 21 ms (9.81%), Space: 55.5 MB (82.86%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-     Set<Integer> seen = new HashSet<>();
-     for(int num : nums) {
-        if (!seen.add(num)){
-            return true;
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) return true;
         }
-     }
-     return false;
+        return false;
     }
 }

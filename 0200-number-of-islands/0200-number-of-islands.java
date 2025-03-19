e5579bcaf233d66f77b51d28c61722a813a5f9d7
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-03-19 08:52:30 +0530

    Time: 8 ms (14.98%), Space: 48.7 MB (98.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,25 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int numberOfIsland = 0;
+        int count = 0;
+        int[][] dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1'){
-                    numberOfIsland++;
-                    dfs(grid, i, j);
+                    count++;
+                    grid[i][j] = '0';
+                    queue.offer(new int[]{i,j});
+                    while(!queue.isEmpty()) {
+                        int[] cell = queue.poll();
+                        for (int[] dir: dirs) {
+                            int x = cell[0] + dir[0];
+                            int y = cell[1] + dir[1];
+                            if (x >= 0 && y >= 0 && x < grid.length && y < grid[0].length) {
+                                if (grid[x][y] == '1') {
+                                    queue.offer(new int[]{x,y});
+                                    grid[x][y] = '0';
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-        return numberOfIsland;
-    }
-
-    private void dfs(char[][] grid, int i, int j){
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
-            return;
-        }
-        grid[i][j] = '0';
-        dfs(grid, i-1, j);
-        dfs(grid, i+1, j);
-        dfs(grid, i, j-1);
-        dfs(grid, i, j+1);
+        return count;
     }
 }

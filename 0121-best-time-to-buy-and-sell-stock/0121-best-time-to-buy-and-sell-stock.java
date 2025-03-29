0710667b7fd4c9ade4716eea1b938e73add9a370
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-03-29 11:43:18 +0530

    Time: 1 ms (99.83%), Space: 61.5 MB (73.87%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
         for (int left = 0, right = 1; right < prices.length; right++) {
-            if (prices[left] < prices[right]) {
+            if (prices[right] > prices[left]) {
                 maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
             } else {
                 left = right;

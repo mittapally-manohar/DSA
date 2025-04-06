97537ce927a52ef5e804bf27b34c9cb8429f8f9c
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-04-06 17:51:56 +0530

    Time: 3 ms (42.06%), Space: 41.7 MB (82.07%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,14 +1,14 @@
 class Solution {
+    Map<Character,Character> map = Map.of(
+        '(',')','[',']','{','}'
+    );
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        Map<Character, Character> map = Map.of(
-                '(', ')',
-                '[', ']',
-                '{', '}');
-        for (char c : s.toCharArray()) {
-            if (map.containsKey(c)) {
-                stack.push(map.get(c));
-            } else if (stack.isEmpty() || stack.pop() != c) {
+        for (char ch : s.toCharArray()){
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(map.get(ch));
+            }
+            else if (stack.isEmpty() || stack.pop()!=ch) {
                 return false;
             }
         }

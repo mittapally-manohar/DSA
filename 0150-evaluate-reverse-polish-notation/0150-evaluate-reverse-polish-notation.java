41a0b2c76c61e480be84bef3c696f97acd47e4e9
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-04-06 17:39:16 +0530

    Time: 6 ms (75.07%), Space: 45.1 MB (17.2%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,28 +1,24 @@
 class Solution {
     private static final Set<String> OPERATORS = Set.of("+", "-", "*", "/");
 
-    private boolean isOperator(String s) {
+    public boolean isOperator(String s) {
         return OPERATORS.contains(s);
     }
 
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
-        for (String s : tokens) {
-            if (!isOperator(s)) {
-                stack.push(Integer.parseInt(s));
-            } else {
-                int r = stack.pop();
-                int l = stack.pop();
-                if (s.equals("+")) {
-                    stack.push(l + r);
-                } else if (s.equals("-")) {
-                    stack.push(l - r);
-                } else if (s.equals("*")) {
-                    stack.push(l * r);
-                } else if (s.equals("/")) {
-                    stack.push(l / r);
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int right = stack.pop();
+                int left = stack.pop();
+                switch (token) {
+                    case "+" -> stack.push(left + right);
+                    case "-" -> stack.push(left - right);
+                    case "*" -> stack.push(left * right);
+                    case "/" -> stack.push(left / right);
                 }
-
+            } else {
+                stack.push(Integer.parseInt(token));
             }
         }
         return stack.pop();

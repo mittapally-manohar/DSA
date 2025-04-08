6e89458532be79ebcefe8ee0368297f0814db9c7
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-04-08 21:58:01 +0530

    Time: 8 ms (3.99%), Space: 42.6 MB (6.02%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -6,10 +6,12 @@ class Solution {
         }
         Map<Object,Object> map = new HashMap<>();
         for (Integer i = 0; i < words.length; i++) {
+            System.out.println(map);
             if (map.put(pattern.charAt(i),i) != map.put(words[i],i)){
                 return false;
             }
         }
+         System.out.println(map);
         return true;
     }
 }

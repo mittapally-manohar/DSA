c5e22e893a844335384892c3a989d69d9cf0cac3
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-04-08 18:38:27 +0530

    Time: 32 ms (44.62%), Space: 51.3 MB (92.67%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
         Arrays.sort(nums);
-        int n= nums.length;
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-            int j = i + 1;
-            int k = n - 1;
+        for (int i = 0; i < n-2; i++){
+            if (i!=0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int j = i+1;
+            int k = n-1;
+            //-4 -1 -1 0 1 2
             while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+                int sum = nums[i]+nums[j]+nums[k];
                 if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[j],nums[k]));
+                    res.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     while (j < k && nums[++j] == nums[j-1]){}
                     while (j < k && nums[--k] == nums[k+1]){}
-                } else if ( sum > 0) {
-                    k--;
-                } else {
-                    j++;
+                }else if (sum > 0) {
+                   k--;
+                }else {
+                   j++;
                 }
             }
         }

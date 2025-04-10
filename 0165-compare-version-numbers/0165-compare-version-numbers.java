3eda62d2c43525df589552b031c7a4d1eb9009e2
Manohar Mittapally <46250057+mittapally-manohar@users.noreply.github.com>
2025-04-10 09:48:05 +0530

    Time: 11 ms (5.64%), Space: 42.1 MB (5.84%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int i = 0, j = 0;
+         System.out.println(v1.length+" "+v2.length);
+        while (i < v1.length && j < v2.length) {
+            int l = Integer.parseInt(v1[i]);
+            int r = Integer.parseInt(v2[i]);
+            System.out.println(l+" "+r);
+            if (l == r) {
+                i++;
+                j++;
+            } else if (l > r) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+        if (i==v1.length && j == v2.length) {
+            return 0;
+        }
+        while (j < v2.length) {
+            int num = Integer.parseInt(v2[j]);
+            if (num == 0) {
+                j++;
+            }else{
+                return -1;
+            }
+        }
+         while (i < v1.length) {
+            int num = Integer.parseInt(v1[i]);
+            if (num == 0) {
+                i++;
+            }else{
+                return 1;
+            }
+        }
+        if (i==v1.length && j == v2.length) {
+            return 0;
+        }
+        return 0;
+    }
+}
